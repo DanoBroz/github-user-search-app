d8be8fb2a52d1cb7fa9dc9627d76b10a4d87a5d5
Daniel Brož <danobroz66@gmail.com>
2022-07-31 17:16:53 +0200

    add dark mode implementation

--- src/hooks/useDarkmode.ts
+++ src/hooks/useDarkmode.ts
@@ -2,15 +2,17 @@ import { useEffect, useState } from "react"
 
 export const useDarkmode = () =>
 {
-    const [theme, setTheme] = useState('dark')
+    const [theme, setTheme] = useState(localStorage.theme)
     const colorTheme = theme === 'dark' ? 'light' : 'dark';
 
     useEffect(() =>
     {
         const root = window.document.documentElement
 
+        root.classList.remove(colorTheme)
         root.classList.add('dark')
-    }, [theme])
+        localStorage.setItem('theme', theme)
+    }, [colorTheme, theme])
 
     return { colorTheme, setTheme }
 }
