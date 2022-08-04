4da2096d6252ce4aa678264b85f065ebcde2ceb5
Daniel Brož <danobroz66@gmail.com>
2022-08-04 21:25:48 +0200

    make reausable containers and components working

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -6,7 +6,7 @@ import { UserImage } from './UserImage'
 export const UserCard = (userData: DefaultData) => {
     return (
         <div className='user-card p-12 shadow-light bg-white rounded-[10px] dark:bg-[#1E2A47] dark:shadow-none'>
-            {userData?.avatar_url && <UserImage avatarUrl={userData?.avatar_url} className='user-image' />}
+            {userData.avatar_url && <UserImage avatarUrl={userData?.avatar_url} className='user-image' />}
             <div className='user-main transition-colors flex mb-[33px] sm:mb-[24px] md:mb-[20px]'>
                 {userData?.avatar_url && <UserImage avatarUrl={userData?.avatar_url} className='user-image--mobile mr-[41px]' />}
                 <div className='flex flex-col md:w-full md:flex-row md:justify-between'>
@@ -16,14 +16,16 @@ export const UserCard = (userData: DefaultData) => {
                         </span>
                         <span className='text-[#0079FF]'>@{userData?.login}</span>
                     </div>
-                    <span className='text-[#4B6A9B] dark:text-white'>Joined {new Date(userData?.created_at).toDateString()}</span>
+                    <span className='text-[#4B6A9B] dark:text-white md:pt-2'>
+                        Joined {new Date(userData?.created_at).toDateString()}
+                    </span>
                 </div>
             </div>
             <p className='user-bio transition-colors text-[#4B6A9B] dark:text-white font-light mb-[23px] sm:mb-[32px] text-[13px] sm:text-[15px]'>
                 {userData?.bio}
             </p>
-            <UserFollowUp className='followup' />
-            <UserLinks className='user-links' />
+            <UserFollowUp {...userData} />
+            <UserLinks {...userData} />
         </div>
     )
 }
--- src/containers/UserFollowUp.tsx
+++ src/containers/UserFollowUp.tsx
@@ -1,20 +1,23 @@
-import { HTMLAttributes } from 'react'
+import { DefaultData } from '../App'
 
-export const UserFollowUp = ({ className }: HTMLAttributes<HTMLDivElement>) =>
-{
+type FollowUpProps = Pick<DefaultData, 'followers' | 'following' | 'public_repos'>
+
+export const UserFollowUp = (userData: FollowUpProps) => {
     return (
-        <div className={`bg-[#F6F8FF] dark:bg-[#141D2F] grid grid-cols-3 py-[18px] px-[32px] rounded-[10px] w-full mb-[30px] ${className}`}>
-            <div className="flex flex-col text-center sm:text-left">
-                <span className="pb-2 text-[#4B6A9B] dark:text-white text-[13px]">Repos</span>
-                <span className="font-bold text-[#2B3442] dark:text-white text-[22px]">8</span>
+        <div
+            className={`bg-[#F6F8FF] dark:bg-[#141D2F] grid grid-cols-3 py-[18px] px-[32px] rounded-[10px] w-full mb-[30px] followup`}
+        >
+            <div className='flex flex-col text-center sm:text-left'>
+                <span className='pb-2 text-[#4B6A9B] dark:text-white text-[13px]'>Repos</span>
+                <span className='font-bold text-[#2B3442] dark:text-white text-[22px]'>{userData?.public_repos}</span>
             </div>
-            <div className="flex flex-col text-center sm:text-left">
-                <span className="pb-2 text-[#4B6A9B] dark:text-white text-[13px]">Followers</span>
-                <span className="font-bold text-[#2B3442] dark:text-white text-[22px]">3938</span>
+            <div className='flex flex-col text-center sm:text-left'>
+                <span className='pb-2 text-[#4B6A9B] dark:text-white text-[13px]'>Followers</span>
+                <span className='font-bold text-[#2B3442] dark:text-white text-[22px]'>{userData?.followers}</span>
             </div>
-            <div className="flex flex-col text-center sm:text-left">
-                <span className="pb-2 text-[#4B6A9B] dark:text-white text-[13px]">Following</span>
-                <span className="font-bold text-[#2B3442] dark:text-white text-[22px]">9</span>
+            <div className='flex flex-col text-center sm:text-left'>
+                <span className='pb-2 text-[#4B6A9B] dark:text-white text-[13px]'>Following</span>
+                <span className='font-bold text-[#2B3442] dark:text-white text-[22px]'>{userData?.following}</span>
             </div>
         </div>
     )
